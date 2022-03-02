5440c61502ae8d078eec1963fa3ce2f6b388fb56
skbiswal01 <97391127+skbiswal01@users.noreply.github.com>
2022-03-02 21:22:23 +0530

    created first npm package

---
+++ day-1_Assignment/main.js
@@ -0,0 +1,11 @@
+const addition = require("./add.js");
+addition(5, 6);
+
+const subtraction = require("./subtract.js");
+subtraction(1000, 200);
+
+const Multiplication = require("./multiply.js");
+Multiplication(200, 200);
+
+const Division = require("./divide.js");
+Division(1000, 10);
