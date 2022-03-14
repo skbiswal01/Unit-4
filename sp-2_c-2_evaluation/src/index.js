0bea4c94bf7bc036dc9f771f26a3f50d61dae419
skbiswal01 <97391127+skbiswal01@users.noreply.github.com>
2022-03-14 13:36:51 +0530

    c2-evaluation

--- sp-2_c-2_evaluation/src/index.js
+++ sp-2_c-2_evaluation/src/index.js
@@ -1,3 +1,15 @@
 const express = require("express");
+
+const fixedController = require("./controllers/fixedcontroller");
+const masterController = require("./controllers/mastercontroller");
+const savingController = require("./controllers/savingcontroller");
+
 const app = express();
+app.use(express.json());
+
+
+app.use("/fixed", fixedController); // /users/abcd/fkhsdkfh/fkhsdk
+app.use("/master",  masterController);
+app.use("/saving", savingController);
+
 module.exports = app;
