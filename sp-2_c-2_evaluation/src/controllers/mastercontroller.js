8d842f84ad01188c553288191efee0917419d4c6
skbiswal01 <97391127+skbiswal01@users.noreply.github.com>
2022-03-14 13:26:38 +0530

    c-2 evalauation

---
+++ sp-2_c-2_evaluation/src/controllers/mastercontroller.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const fixedaccount = require("../model/fixedaccount.model");
+const router = express.Router();
+
+router.post("/", async(req,res)=>{
+    try {
+        const Fixedacc=await fixedacc.find().lean().exec();
+        return res.status(200).send(Fixedacc)
+    } catch (error) {
+        res.status(500).send({message:error.message})
+    }
+})
