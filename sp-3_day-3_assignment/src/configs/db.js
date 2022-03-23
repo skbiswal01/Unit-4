de28fc3be3a7d2fc3c4f5d969a7118c225c1505c
skbiswal01 <97391127+skbiswal01@users.noreply.github.com>
2022-03-23 19:53:29 +0530

    authentication completed

--- sp-3_day-3_assignment/src/configs/db.js
+++ sp-3_day-3_assignment/src/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = () =>{
-    mongoose.connect("mongodb://127.0.0.1:27017/authenctication");
+    mongoose.connect("mongodb://127.0.0.1:27017/authentication");
 }
--- sp-3_day-3_assignment/src/index.js
+++ sp-3_day-3_assignment/src/index.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const connect = require("./configs/db");
+const userController = require("./controllers/user.controller")
+const postController = require("./controllers/Post.controller")
 
+const {register,login} = require("./controllers/auth.controller")
 const app = express();
+
+app.use(express.json());
+
+
+app.use("/users", userController)
+
+app.post("/register", register)
+
+app.post("/login", login)
+
+app.use("/posts", postController)
 
 module.exports = app;
