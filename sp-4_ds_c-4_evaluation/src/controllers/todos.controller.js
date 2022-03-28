311cda252010e223a318e1740c8f13442a61588f
skbiswal01 <97391127+skbiswal01@users.noreply.github.com>
2022-03-28 11:04:58 +0530

    Update todos.controller.js

--- sp-4_ds_c-4_evaluation/src/controllers/todos.controller.js
+++ sp-4_ds_c-4_evaluation/src/controllers/todos.controller.js
@@ -23,4 +23,39 @@ router.get("", async(req, res) =>{
     }
 })
 
+router.get("/:id", async(req, res) =>{
+    try{
+        const todos = await Todos.find();
+        return res.status(200).send(todos);
+    }catch(err){
+        return res.status(400).send({message: err.message});
+    }
+})
+
+router.delete("/:id", authenticate, async (req, res) => {
+
+    try{
+        const todo = await todo.findByIdAndDelete(req.params.id);
+        return res.status(200).send(todo);
+    }
+    catch(err){
+        return res.status(400).send({message : err.message})
+    }
+ 
+});
+
+router.patch("/:id", authenticate, async (req, res) => {
+
+    try{
+        const todo = await todo.findByIdAndUpdate(req.params.id, req.body, {
+            new : true
+        })
+        return res.status(200).send(todo);
+    }
+    catch(err){
+        return res.status(400).send({message : err.message})
+    }
+ 
+});
+
 module.exports = router;
